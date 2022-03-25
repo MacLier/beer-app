71d3961a708a4b6048f0a1e8aee57c273dbd9eaa
MacLier <75802094+MacLier@users.noreply.github.com>
2022-03-25 09:56:08 +0100

    feat: basic app created & fetch data

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+<p>{{info}}</p>
+  <OneBeer></OneBeer>
+</template>
+
+<script>
+import OneBeer from './components/OneBeer.vue'
+import axios from 'axios'
+
+export default {
+  name: 'App',
+  components: {
+    OneBeer
+  },
+  data () {
+    return {
+      info: null
+    }
+  },
+  mounted () {
+    axios
+      .get('https://api.punkapi.com/v2/beers')
+      .then((response) => (this.info = response.data))
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
