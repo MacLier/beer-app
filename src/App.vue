7778db557ec25e6624fdcbee2ad6a4dc390615da
MacLier <75802094+MacLier@users.noreply.github.com>
2022-03-25 10:16:04 +0100

    feat: Beers listed with child elements

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <template>
+<header>
+<h1>Sexy Crafted Beers</h1>
+</header>
+<ul>
+  <OneBeer
+  v-for="beer in beers"
+  :key="beer.id"
+  :id="beer.id"
+  :name="beer.name"
+  :image-url="beer.image_url"
+  ></OneBeer>
+</ul>
 <p>{{info}}</p>
-  <OneBeer></OneBeer>
 </template>
 
 <script>
@@ -14,13 +25,13 @@ export default {
   },
   data () {
     return {
-      info: null
+      beers: []
     }
   },
   mounted () {
     axios
       .get('https://api.punkapi.com/v2/beers')
-      .then((response) => (this.info = response.data))
+      .then((response) => (this.beers = response.data))
   }
 }
 </script>
--- src/components/OneBeer.vue
+++ src/components/OneBeer.vue
@@ -1,10 +1,27 @@
 <template>
-  <div>{{ info }}</div>
+  <li :style="{'background-image': 'url(' + imageUrl +')'}"
+  class="card">
+  <h2>{{name}}</h2>
+  </li>
 </template>
 
 <script>
 
 export default {
+  props: {
+    id: {
+      type: Number,
+      required: true
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    imageUrl: {
+      type: String,
+      required: true
+    }
+  },
   data () {
     return {
     }
